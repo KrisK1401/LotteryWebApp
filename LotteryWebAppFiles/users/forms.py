b737d63bee45b6333da89ff4275abc9052f74413
Chris1401 <k.kalcic1@newcastle.ac.uk>
2021-10-25 22:39:11 +0100

    Validation for lenght of the password using Jinja control flow statement in register.html and defined parameter stating the min and max number of characters

--- LotteryWebAppFiles/users/forms.py
+++ LotteryWebAppFiles/users/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, Length
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     firstname = StringField(validators=[Required()])
     lastname = StringField(validators=[Required()])
     phone = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and 12 characters in length.')])
     confirm_password = PasswordField(validators=[Required()])
     pin_key = StringField(validators=[Required()])
     submit = SubmitField()
