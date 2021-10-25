5b4be3e72088ce72ec9e13e0a990b9dbec9f2654
Chris1401 <k.kalcic1@newcastle.ac.uk>
2021-10-25 23:51:46 +0100

    password pattern matching, set the password as needing to have at least 1 digit, 1 uppercase letter,1 lowercase letter, and 1 special character

--- LotteryWebAppFiles/users/forms.py
+++ LotteryWebAppFiles/users/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, Length, ValidationError
+import re
+
 
 def character_check(form,field):
     excluded_chars = "*?!'^+%&/()={[]}$#@<>"
@@ -16,6 +18,11 @@ class RegisterForm(FlaskForm):
     lastname = StringField(validators=[Required()])
     phone = StringField(validators=[Required()])
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and 12 characters in length.')])
-    confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both password fields must be equal!')])
+    confirm_password = PasswordField(validators=[Required()])
     pin_key = StringField(validators=[Required()])
     submit = SubmitField()
+
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W)')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit, 1 upper case, 1 lower case, and 1 special character.")
